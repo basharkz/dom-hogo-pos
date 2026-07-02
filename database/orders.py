9316895946b84fa3cb00f3515c9747ab8f70c71a
basharkz <basharalkazak@hotmail.com>
2026-07-02 05:39:47 +0500

    fix order

--- database/orders.py
+++ database/orders.py
@@ -7,7 +7,7 @@ def db_get_active_orders():
 
 def db_get_order_by_id(order_id):
     row = execute_query("SELECT order_id, order_name, cart_json, discount_percent FROM active_orders WHERE order_id = %s", (order_id,), fetch="one")
-    return {"id": row[0], "name": row[1], "cart": json.loads(row[2]), "discount": row[3]} if row else None
+    return {"id": row[0], "name": row[1], "cart": row[2], "discount": row[3]} if row else None
 
 def db_update_order(order_dict):
     execute_query("UPDATE active_orders SET order_name = %s, cart_json = %s, discount_percent = %s WHERE order_id = %s",
